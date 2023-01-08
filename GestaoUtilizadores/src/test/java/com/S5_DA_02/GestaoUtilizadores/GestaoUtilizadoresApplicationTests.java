8e49532eff6ada50cbfd9c004f9eb88de6972e18
1190624 <1190624@isep.ipp.pt>
2023-01-08 16:12:40 +0000

    Setup do Projeto;
    LEI-167 #comment Setup do Projeto "S5_DA_02-GestaoUtilizadores";

---
+++ GestaoUtilizadores/src/test/java/com/S5_DA_02/GestaoUtilizadores/GestaoUtilizadoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.S5_DA_02.GestaoUtilizadores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoUtilizadoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
