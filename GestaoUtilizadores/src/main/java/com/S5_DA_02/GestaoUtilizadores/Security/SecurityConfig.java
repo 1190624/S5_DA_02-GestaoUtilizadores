61551f22f5501b03a262d1970afd8181adf70d68
1190624 <1190624@isep.ipp.pt>
2023-01-08 16:23:25 +0000

    Commit Classe Configuração "SecurityConfig";
    LEI-167 #comment Commit Classe Configuração "SecurityConfig";

---
+++ GestaoUtilizadores/src/main/java/com/S5_DA_02/GestaoUtilizadores/Security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.S5_DA_02.GestaoUtilizadores.Security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/auth/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2Login();
+
+        return http.build();
+    }
+}
